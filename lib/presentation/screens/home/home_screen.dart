cf7912dc19fb1e2d69b98f9a44858fc0bb62b34e
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-09-26 12:13:14 -0500

    home screen create

---
+++ lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('buenass')),
+    );
+  }
+}
