6c86ed7fcc473582a863823c21553bdceb42e25f
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-09-26 13:49:37 -0500

    router with name

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String name = 'home_screen';
+
   const HomeScreen({super.key});
 
   Widget build(BuildContext context) {
@@ -55,10 +58,11 @@ class _CustomListTile extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
       onTap: () {
-        context.push(menuItem.link);
         /*Navigator.of(context).push(
             MaterialPageRoute(builder: (context) => const ButtonsScreen()));*/
         //Navigator.pushNamed(context, menuItem.link);
+        context.push(menuItem.link);
+        //context.pushNamed(CardsScreen.name);
       },
     );
   }
