c874a534b47cf69d2cdcfb845c5899e324c2d64c
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-09-26 13:11:44 -0500

    ruoter basic

---
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CardsScreen extends StatelessWidget {
+  const CardsScreen({super.key});
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('cards'),
+      ),
+      body: Placeholder(),
+    );
+  }
+}
