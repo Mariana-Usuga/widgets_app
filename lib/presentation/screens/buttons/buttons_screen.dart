3e72d739b48d1f23001aa347a1fedd0f0205273e
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-09-26 13:42:29 -0500

    go_router configured

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 
 class ButtonsScreen extends StatelessWidget {
   const ButtonsScreen({super.key});
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Buttons'),
       ),
-      body: Placeholder(),
+      body: Text('hi'),
     );
   }
 }
